15a884b829ebdbd00e25d4d8f4b36cf5df6c2ba2
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-09-01 20:27:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //Your code here
+
+function justInvoke(func){
+  return func();
+}
+
+function setThisWithCall(fn, thisValue, arg){
+  return fn.call(thisValue, arg);
+}
+
+const setThisWithApply= function(fn, thisValue, arg){
+  return fn.apply(thisValue,arg)
+}
+
+function returnNewFunctionOf(fn, thisValue){
+  func=fn.bind(thisValue);
+  return func
+}
